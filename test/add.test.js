6810c8000606f4554e5d4ffff924ad0ec84eee88
Delano Francis <deltel000@gmail.com>
2023-04-25 23:42:14 -0500

    Add subtraction case

--- test/add.test.js
+++ test/add.test.js
@@ -1,7 +1,7 @@
 describe("Arithmetic operations on floating-point numbers in JavaScript are not always accurate", () => {
-    test("addition fails", () => {
+    test("addition fails with rounding error", () => {
         const sum = 7767674.28 + 3348656
-        expect(sum).toBe(11116330.28);
+        expect(sum).toBe(11116330.280000001);
     });
 
     test("addition successful", () => {
