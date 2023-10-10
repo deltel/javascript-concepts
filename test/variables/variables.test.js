6ecc053369dab70de7d87b9ce978fd38df0e0d2e
Delano Francis <deltel000@gmail.com>
2023-10-09 22:53:22 -0500

    Add variable hoisting test

--- test/variables/variables.test.js
+++ test/variables/variables.test.js
@@ -1,13 +1,13 @@
 describe("variables", () => {
     describe("block scope", () => {
-        test.failing("applies to let and const", () => {
+        test("applies to let and const", () => {
             if(true) {
                 let x = "x";
                 const y = "y";
             }
 
-            expect(x).toBeUndefined();
-            expect(y).toBeUndefined();
+            expect(() => x).toThrow(ReferenceError);
+            expect(() => y).toThrow(ReferenceError);
         });
         
         test("does not apply to var", () => {
@@ -16,6 +16,25 @@ describe("variables", () => {
             }
 
             expect(x).toBe("x");
+        });
+    });
+    
+    describe("hoisting", () => {
+        test("is immaterial for \"let\" and \"const\"", () => {
+            let x = 1;
+            const y = 2;
+
+            (function() {
+                expect(() => x).toThrow(ReferenceError); // notice that x is ont 1
+                expect(() => y).toThrow(ReferenceError); // and y is not 2
+                let x = "x";
+                const y = "y";
+            })();
+        });
+        
+        test("var declarations are hoisted", () => {
+            expect(x).toBeUndefined();
+            var x = "x";
         });
     });
 });
